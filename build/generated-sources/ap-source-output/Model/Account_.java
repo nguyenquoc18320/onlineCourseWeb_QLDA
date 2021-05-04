c9b04a8df616e2813cf14a5c4b400b2295775ffb
Anh Quốc <nguyenquoc18320@.gmail.com>
2021-05-04 13:19:33 +0700

    all

---
+++ build/generated-sources/ap-source-output/Model/Account_.java
@@ -0,0 +1,15 @@
+package Model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2021-03-15T21:14:58")
+@StaticMetamodel(Account.class)
+public class Account_ { 
+
+    public static volatile SingularAttribute<Account, Boolean> Status;
+    public static volatile SingularAttribute<Account, Integer> AccountId;
+    public static volatile SingularAttribute<Account, String> Password;
+
+}
---
+++ build/generated-sources/ap-source-output/Model/PartFiles_.java
@@ -0,0 +1,16 @@
+package Model;
+
+import Model.Part;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2021-03-15T21:14:58")
+@StaticMetamodel(PartFiles.class)
+public class PartFiles_ { 
+
+    public static volatile SingularAttribute<PartFiles, String> fileDocument;
+    public static volatile SingularAttribute<PartFiles, Part> part;
+    public static volatile SingularAttribute<PartFiles, Integer> id;
+
+}
